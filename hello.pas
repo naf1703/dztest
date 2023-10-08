6d42c1d3056a7baef203515cbd84345ce06c26e4
naf1703 <chetvertakovmaxim@gmail.com>
2023-10-08 16:39:02 +0300

    language

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  print('hello,world');
+  Writeln('На каком языке вы говорите?');
+  var lang:=readstring;
+  if lang='русский' then
+    print('Привет!')
+  else if lang='английский' then
+    print('hello!')
+  else if lang='белорусский' then
+    print('прывітанне');
 end.
